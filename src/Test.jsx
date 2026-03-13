c086a1e6f0a9a6224303f5fe8b2bf89fa65efa0c
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2026-03-13 01:37:59 -0300

    fetch succesful

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,7 +1,71 @@
+import { useState , useEffect } from "react";
+
 function Test() {
+
+  const [done, setDone] = useState(false);
+  const [imgSrc, setImgSrc] = useState(null);
+
+  console.log(imgSrc);
+  
+
+    // async function logWeather() {
+
+    //     try {
+
+    //     const response = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Decode%20Talker`);
+
+    //     let weatherData = await response.json();+
+
+    //     console.log(weatherData);
+        
+
+    //     return weatherData;
+
+    //     } catch {
+
+    //         console.log('error');
+
+    //     }
+
+    // };
+
+    // let myResult = logWeather();
+
+  useEffect(() => {
+
+    async function getCards() {
+
+      try {
+        console.log('trying');
+        
+
+        const response = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Decode%20Talker`);
+        const cardData = await response.json();
+        console.log(cardData.data[0].card_images[0].image_url);
+        setImgSrc(cardData.data[0].card_images[0].image_url);
+        
+      } catch (error) {
+        console.error(error);
+      }
+
+    }
+
+    if (done == false) {
+      getCards();
+      setDone(true);
+    }
+
+  }, [done])
+
+  
+
+
+
     return (
         <div>
             <p>TESTINGGGG</p>
+            <p>My result is: {}!!!</p>
+            <img src={imgSrc}></img>
         </div>
     )
 }
