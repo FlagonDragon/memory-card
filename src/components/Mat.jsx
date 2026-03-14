fc0bc3e939ccc8622638aa14e7150942604ee38a
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2026-03-14 04:40:52 -0300

    add deck shuffle

--- src/components/Mat.jsx
+++ src/components/Mat.jsx
@@ -1,9 +1,12 @@
 import Card from './Card.jsx'
 import '../styles/Mat.css'
+import shuffleArray from '../shuffleArray.js'
 
 function Mat() {
 
     let deck = ['Spright%20Blue', 'Spright%20Blue', 'Toon%20Kingdom', 'Toon%20Kingdom', 'Dark%20Magician', 'Dark%20Magician'];
+
+    shuffleArray(deck);
 
     // let i = 0;
     // while (i < 6) {
