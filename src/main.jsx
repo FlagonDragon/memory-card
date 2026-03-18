6246acc6fed899f6e4e76e8ffd748e9e67b0910a
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2026-03-18 04:53:41 -0300

    clean up files

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import FlipCard from './components/FlipCard.jsx'
 import Mat from './components/Mat.jsx'
 
 createRoot(document.getElementById('root')).render(
