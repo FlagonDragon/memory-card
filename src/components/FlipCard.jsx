e5efecff9c337fca6eb1d1ebd486f61801e794ac
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2026-03-18 02:33:12 -0300

    flipCard prototype done

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -1,22 +1,25 @@
 import backSrc from '../assets/back.jpg';
+import '../styles/FlipCard.css'
 
 function FlipCard() {
 
     return(
         <div class="flip-card">
+
             <div class="flip-card-inner">
 
                 <div class="flip-card-front">
                     <img src={backSrc} alt="back" height='400px' width='274.42px'></img>
                 </div>  
 
-                {/* <div class="flip-card-back">
+                <div class="flip-card-back">
                     <h1>John Doe</h1>
                     <p>Architect & Engineer</p>
                     <p>We love that guy</p>
-                </div> */}
+                </div>
                 
             </div>
+            
         </div>
     )
 
